8388725777fd63d01a9c89a8aaaaa06a4d4f16fa
Nathon Chavez <nathonchavez01@gmail.com>
2021-11-09 15:43:31 +0900

    Finished Project

--- DynamicWebsiteDevelopment/ToDoListProject/script.js
+++ DynamicWebsiteDevelopment/ToDoListProject/script.js
@@ -1,48 +1,45 @@
 $(document).ready(function(){
-  var getAndDisplayAllTasks = function () {
+  var getAndDisplayAllTasks = function (filter) {
+    if (!filter) {
+      filter = 'all';
+    }
+
     $.ajax({
       type: 'GET',
       url: 'https://altcademy-to-do-list-api.herokuapp.com/tasks?api_key=200',
       dataType: 'json',
       success: function (response, textStatus) {
         $('.todo-list').empty();
+        var activeTasks = 0;
         
-        response.tasks.forEach(function (task) {
+        response.tasks.filter(function (task) {
+          if (filter === 'all') {
+            return true;
+          }
+          if (filter === 'active') {
+            return !task.completed;
+            
+          }
+          if (filter === 'completed') {
+            return task.completed;
+          }
+        })
+        .forEach(function (task) {
+          if (!task.completed) {
+            activeTasks++;
+          }
+          
           $('.todo-list').append('<div class="col-xs-12 to-do ' + (task.completed ? 'complete' : '') + '"><input type="checkbox" class="mark-complete" data-id="' + task.id + '"' + (task.completed ? 'checked' : '') + '><p>' + task.content + '</p><button class="delete btn-danger" data-id="' + task.id + '"></button></div>');
+          
         });
+        $('.to-do-amount span').text(activeTasks.length + " task remaining");
       },
-     
       error: function (request, textStatus, errorMessage) {
         console.log(errorMessage);
-      }
+    }
+    
     });
   }
-  
-  var createTask = function () {
-    $.ajax({
-      type: 'POST',
-      url: 'https://altcademy-to-do-list-api.herokuapp.com/tasks?api_key=200',
-      contentType: 'application/json',
-      dataType: 'json',
-      data: JSON.stringify({
-        task: {
-          content: $('#task-content').val()
-        }
-      }),
-      success: function (response, textStatus) {
-        $('#task-content').val('');
-        getAndDisplayAllTasks();
-      },
-      error: function (request, textStatus, errorMessage) {
-        console.log(errorMessage);
-      }
-    });  
-  }
-  
-  $('#create-task').on('submit', function (e) {
-    e.preventDefault();
-    createTask();
-  });
 
   var deleteTask = function (id) {
     $.ajax({
@@ -101,6 +98,17 @@ y = n.getFullYear();
 m = n.getMonth() + 1;
 d = n.getDate();
 document.getElementById("title").innerHTML = "To-do List: " + m + "/" + d + "/" + y;
+
+
+$('#all-button').on('click', function () {
+  getAndDisplayAllTasks('all');
+});
+$('#remaining-button').on('click', function () {
+  getAndDisplayAllTasks('active');
+});
+$('#completed-button').on('click', function () {
+  getAndDisplayAllTasks('completed');
+});
 
 $('all-button').on('click', showAll);
 function showAll() {
