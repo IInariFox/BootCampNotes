b9d545000e0c678fa09ac8fb5f017d7a5f3e63b5
Nathon Chavez <nathonchavez01@gmail.com>
2021-11-09 15:55:02 +0900

    Finished Project

--- DynamicWebsiteDevelopment/ToDoListProject/script.js
+++ DynamicWebsiteDevelopment/ToDoListProject/script.js
@@ -18,7 +18,6 @@ $(document).ready(function(){
           }
           if (filter === 'active') {
             return !task.completed;
-            
           }
           if (filter === 'completed') {
             return task.completed;
@@ -28,18 +27,44 @@ $(document).ready(function(){
           if (!task.completed) {
             activeTasks++;
           }
-          
+
           $('.todo-list').append('<div class="col-xs-12 to-do ' + (task.completed ? 'complete' : '') + '"><input type="checkbox" class="mark-complete" data-id="' + task.id + '"' + (task.completed ? 'checked' : '') + '><p>' + task.content + '</p><button class="delete btn-danger" data-id="' + task.id + '"></button></div>');
-          
         });
-        $('.to-do-amount span').text(activeTasks.length + " task remaining");
+        $('.to-do-amount').text(activeTasks.length);
+        console.log(activeTasks)
+      },
+     
+      error: function (request, textStatus, errorMessage) {
+        console.log(errorMessage);
+      }
+    });
+  }
+  
+  var createTask = function () {
+    $.ajax({
+      type: 'POST',
+      url: 'https://altcademy-to-do-list-api.herokuapp.com/tasks?api_key=200',
+      contentType: 'application/json',
+      dataType: 'json',
+      data: JSON.stringify({
+        task: {
+          content: $('#task-content').val()
+        }
+      }),
+      success: function (response, textStatus) {
+        $('#task-content').val('');
+        getAndDisplayAllTasks();
       },
       error: function (request, textStatus, errorMessage) {
         console.log(errorMessage);
-    }
-    
-    });
+      }
+    });  
   }
+  
+  $('#create-task').on('submit', function (e) {
+    e.preventDefault();
+    createTask();
+  });
 
   var deleteTask = function (id) {
     $.ajax({
@@ -93,35 +118,21 @@ $(document).ready(function(){
       markTaskActive($(this).data('id'));
     }
   });
-n =  new Date();
-y = n.getFullYear();
-m = n.getMonth() + 1;
-d = n.getDate();
-document.getElementById("title").innerHTML = "To-do List: " + m + "/" + d + "/" + y;
+  n =  new Date();
+  y = n.getFullYear();
+  m = n.getMonth() + 1;
+  d = n.getDate();
+  document.getElementById("title").innerHTML = "To-do List: " + m + "/" + d + "/" + y;
 
-
-$('#all-button').on('click', function () {
-  getAndDisplayAllTasks('all');
-});
-$('#remaining-button').on('click', function () {
-  getAndDisplayAllTasks('active');
-});
-$('#completed-button').on('click', function () {
-  getAndDisplayAllTasks('completed');
-});
-
-$('all-button').on('click', showAll);
-function showAll() {
-  $('all-button').toggleClass('todo-list');
-}
-$('remaining-button').on('click', showRemaining);
-function showRemaining() {
-  $('remaining-button').toggleClass('remaining');
-}
-$('completed-button').on('click', showCompleted);
-function showCompleted() {
-$('completed-button').toggleClass('mark-complete');
-}
+  $('#all-button').on('click', function () {
+    getAndDisplayAllTasks('all');
+  });
+  $('#remaining-button').on('click', function () {
+    getAndDisplayAllTasks('active');
+  });
+  $('#completed-button').on('click', function () {
+    getAndDisplayAllTasks('completed');
+  });
 
 function filteredItems() {
   $(this).addClass('active');
